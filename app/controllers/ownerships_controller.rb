27aabb73586501a240d359c732c36a7b20c0fe6c
aki.yu <yommme373@gmail.com>
2017-11-03 08:00:47 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,17 +14,26 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    # Have 関係として保存   
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
     end
 
     redirect_back(fallback_location: root_path)
   end
 
+
   def destroy
     @item = Item.find(params[:item_id])
 
+    # Want 関係として削除
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
     end
 
     redirect_back(fallback_location: root_path)
